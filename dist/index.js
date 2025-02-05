ecc28b70d685299aef936aea297a01534a88a23a
luisatwistedchaos101 <luisaseafox@gmail.com>
2025-02-05 15:46:32 -0300

    Que isso cara

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+"Use scrict";
+//tipando variaveis
+let mensagem = "Hello, typescript!";
+console.log(mensagem);
+//tipando funcoes
+function somar(a, b) {
+    return a + b;
+}
+somar(69, 24);
+// exercício 1 //
+let afirmacao = "Fora Lula";
+let numeroRandom = 42;
+let VontadeDeViver = false;
+//exercicio 2//
+function soma(a, b) {
+    return a + b;
+}
+somar(22, 13);
+//exercicio 3//
+let valorAleatorio = null;
+console.log;
---
+++ src/index.ts
@@ -0,0 +1,38 @@
+"Use scrict"
+
+//tipando variaveis
+let mensagem: string = "Hello, typescript!";
+
+console.log(mensagem);
+
+//tipando funcoes
+function somar(a: number, b: number):number {
+    return a + b
+}
+
+
+somar(69, 24)
+
+// exercício 1 //
+
+let afirmacao: string = "Socorro, Deus"
+
+let numeroRandom: number = 42
+
+let VontadeDeViver: boolean = false;
+
+//exercicio 2//
+
+function soma(a: number, b: number):number{
+    return a + b;
+}
+
+somar(22, 13)
+
+//exercicio 3//
+
+let valorAleatorio: number | null = null;
+
+console.log(valorAleatorio)
+
+valorAleatorio =
